0e201fb47b7f8241161857552cdebf02cde56c1d
Oscar Lopez <oscareduardolp6@gmail.com>
2021-06-19 18:57:12 -0500

    Quedó prototipada la interfaz de la pantalla principal

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -7,12 +7,25 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
-  public folder: string;
-
-  constructor(private activatedRoute: ActivatedRoute) { }
+    public folder: string;
+    public user: string; 
+    public slideOpt
+    public homeWorkIcon: string = 'https://image.flaticon.com/icons/png/512/2702/2702069.png'; 
+    public slides: string [] = [
+        'https://thumbs.dreamstime.com/z/summer-party-horizontal-retro-colored-banner-flyer-girl-singing-microphone-order-ticket-online-annual-musical-festival-event-151816498.jpg', 
+        'https://www.templateupdates.com/wp-content/uploads/2019/05/Horizontal-Conference-Flyer-Design-Template.jpg', 
+        'https://previews.123rf.com/images/korara/korara1708/korara170800160/84999108-grand-opening-vector-illustration-template-banner-or-horizontal-poster-with-abstract-background-for-.jpg'
+    ]
+  constructor(private activatedRoute: ActivatedRoute) { 
+        this.user = "Daniel"
+  }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
+}
+
+interface user{
+    usuario: string; 
 }
